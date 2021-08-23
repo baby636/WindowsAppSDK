605f105c5c63b606d49af8102e662b846837ea31
Eric Langlois <erlangl@microsoft.com>
2021-08-23 09:38:37 -0700

    Using new guid for tracelogging ofReunion

--- dev/PushNotifications/Insight.h
+++ dev/PushNotifications/Insight.h
@@ -7,12 +7,12 @@
 
 class InsightsSample : public wil::TraceLoggingProvider
 {
-    //IMPLEMENT_TRACELOGGING_CLASS(InsightsSample, "Microsoft.WindowsAppSDK.Insights.Sample",
+    IMPLEMENT_TRACELOGGING_CLASS(InsightsSample, "Microsoft.WindowsAppSDK.Insights.Sample",
         // {c782669f-f4db-53f2-9674-13b8897164cc}
-    //    (0xc782669f, 0xf4db, 0x53f2, 0x96, 0x74, 0x13, 0xb8, 0x89, 0x71, 0x64, 0xcc));
+        (0xc782669f, 0xf4db, 0x53f2, 0x96, 0x74, 0x13, 0xb8, 0x89, 0x71, 0x64, 0xcc));
 
     // {1870FBB0-2247-44D8-BF46-B02130A8A477}
-    IMPLEMENT_TRACELOGGING_CLASS(InsightsSample, "Microsoft.Windows.Notifications.WpnApis", (0x1870fbb0, 0x2247, 0x44d8, 0xbf, 0x46, 0xb0, 0x21, 0x30, 0xa8, 0xa4, 0x77));
+    //IMPLEMENT_TRACELOGGING_CLASS(InsightsSample, "Microsoft.Windows.Notifications.WpnApis", (0x1870fbb0, 0x2247, 0x44d8, 0xbf, 0x46, 0xb0, 0x21, 0x30, 0xa8, 0xa4, 0x77));
 
     // Event that contains a single bool value as payload
     DEFINE_COMPLIANT_TELEMETRY_EVENT_BOOL(BooleanTelemetryEvent, // Event name
